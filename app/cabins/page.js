e84658c9a30baa5f2a04f35cf8da8a6310799590
Mohamad-Ajmal <Ajmalibrahimkhail121@gmail.com>
2025-03-11 14:03:55 +0430

    Initial project setup

---
+++ app/cabins/page.js
@@ -0,0 +1,13 @@
+import Counter from "@/app/_components/Counter";
+
+export default function Page(){
+  
+   
+   return (
+        <div>
+            <h1>Cbins Page</h1>
+
+
+        </div>
+    )
+}
---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to Oasis Hotel</h1>
+      <Link href="/cabins">Explore the luxury cabins</Link>
+    </div>
+  );
+}
