7445e5873d1e0a53f2378cfc95aeab1514e4bad9
Mohamad-Ajmal <Ajmalibrahimkhail121@gmail.com>
2025-03-12 10:18:34 +0430

    Adding nested routes and pages

--- app/account/page.js
+++ app/account/page.js
@@ -2,5 +2,7 @@ export const  metadata = {
     title: "Guest area",
 };
 export default function Page(){
-    return <h1>Account Page</h1>
+    return <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+    Welcome, Ajmal
+  </h2>;
 }
