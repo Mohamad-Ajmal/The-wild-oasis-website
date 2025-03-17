157b8bcccb02e39931dc40e13636b9796ed8f543
Mohamad-Ajmal <Ajmalibrahimkhail121@gmail.com>
2025-03-17 14:25:18 +0430

    configuring authnext for authentication

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,6 @@
+import { handler } from "@/app/_lib/auth";  // Import the handler from auth.js
+import { getServerSession } from "next-auth";
+import { authConfig } from "@/app/_lib/auth";
+// Export the handler for GET and POST requests
+export { handler as GET, handler as POST };
+
