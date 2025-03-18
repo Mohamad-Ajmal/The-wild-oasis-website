fcde6db8a2c330e16c558b87dc19abae50484f60
Mohamad-Ajmal <Ajmalibrahimkhail121@gmail.com>
2025-03-18 13:48:09 +0430

    displaying a loding indicator the useFormStatus Hook

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -2,10 +2,13 @@
 "use client"
 import { useState } from "react";
 import { updateGuest } from "../_lib/actions";
+import { useFormStatus } from "react-dom";
 
 export default function UpdateProfileForm({guest,children}) {
-    const [account, setAccount] = useState();
-   const {fullName, email, nationality, nationalID, countryFlag} = guest;
+  const [account, setAccount] = useState();
+  const {fullName, email, nationality, nationalID, countryFlag} = guest;
+
+
   return (
     <form action={updateGuest} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
     <div className="space-y-2">
@@ -51,10 +54,18 @@ export default function UpdateProfileForm({guest,children}) {
     </div>
 
     <div className="flex justify-end items-center gap-6">
-      <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-        Update profile
-      </button>
+      <Button />
     </div>
   </form>
+  )
+}
+
+function Button() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300" disabled={pending}>
+        { pending ? "updating..." :"Update profile"}
+    </button>
   )
 }
